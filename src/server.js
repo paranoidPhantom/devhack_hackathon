7e9d7d7d26552a9c53d59a12d8db5f01632598ea
Andrei <andrei.hudalla@outlook.com>
2023-02-17 20:11:56 +0300

    Got camera input added

---
+++ src/server.js
@@ -0,0 +1,95 @@
+const http = require("http")
+const fs = require("fs")
+const tgAPI = require('node-telegram-bot-api');
+const path = require("path")
+const port = 3000
+var secrets = JSON.parse(fs.readFileSync(__dirname+'/secrets.json'))
+var gCONFIG = JSON.parse(fs.readFileSync(__dirname+'/config.json'))
+var colors = require('colors');
+
+const bot = new tgAPI(secrets.telegramBotToken, {polling: true});
+
+function sendRes(url, contentType, response){
+    const file = path.join(__dirname+"/",url)
+    fs.readFile(file,(error,content) => {
+        if (error){
+            response.writeHead(404)
+            response.write("File not found | 404")
+            response.end()
+            console.log("Could not find file: "+file)
+        } else {
+            try {
+                if (JSON.parse(content).isSecret == true) {
+                    response.writeHead(403)
+                    response.write("You do not have permission to view this | 403")
+                    response.end()
+                    console.log("Returned 403: file is secret")
+                    response = null
+                    content = null
+                    return
+                }
+            } catch (error) {}
+            response.writeHead(200, {'Content-Type' : contentType})
+            response.write(content)
+            response.end()
+            console.log("Got file: " + file)
+        }
+    })
+}
+
+function getContentType(url){
+    switch (path.extname(url)){
+        case ".html": return "text/html"
+        case ".css": return "text/css"
+        case ".js": return "text/javascript"
+        case ".json": return "application/json"
+        case ".svg": return "image/svg"
+        case ".png": return "image/png"
+        default: return "application/octate-stream"
+    }
+}
+
+const server = http.createServer(function(request,response){
+    if (request.url === "/"){
+        sendRes("index.html","text/html",response)
+    } else {
+        sendRes(request.url, getContentType(request.url), response)
+    }
+})
+
+server.listen(port, function(error){
+    if (error) {
+        console.log("Something went wrong!", error)
+    } else{
+        console.log(("Server is listening on port "+ port).rainbow)
+    }
+})
+
+
+// BOT starts here:
+
+bot.setMyCommands([
+    {
+        command: "/start",
+        description: "Стартовая команда"
+    }
+])
+
+bot.onText("/start", (msg, match) => {
+    const chatID = msg.chat.id
+    const actionOptions = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{
+                    text: "Проверить ссылку",
+                    callback_data: "check_link"
+                }]
+            ]
+        })
+    }
+    bot.sendMessage(chatID, "Привет, что бы ты хотел сделать?")
+})
+
+bot.on("callback_query", msg => {
+    console.log(msg.data)
+})
